df9b01b04078c0aa093fd8e39fdc1cdce5cdcf9d
Gurpal929 <https://github.com/Gurpal929/Mvc.git>
2023-03-03 23:03:14 +0530

    update

--- src/main/java/com/sahota/LoginServlet.java
+++ src/main/java/com/sahota/LoginServlet.java
@@ -6,35 +6,45 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Servlet implementation class LoginServlet
  */
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
+       /**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	   //Creating objects  
+		Dao dao=new Dao();
+	    GetSet gs=new GetSet();
+	    //Establishing Connection to database
+	    dao.getConnection();
+	    //Getting values from ForntEnd
+	    String uname=gs.getUsername();
+	    String pass=gs.getPassword();
+	    //
+	    PreparedStatement ps;
+		try {
+			ps = dao.con.prepareStatement("select username from gmart username=? and passwors=?");
+			   ps.setString(1, uname);
+			   ps.setString(2, pass);
+			   
+			    
+			    
+			    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	 
+	    
+	 
+	    
+		
 	}
 
 }
